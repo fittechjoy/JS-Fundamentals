4f96fa5196df1badf82b2cc5c40e2aa839fc2e6c
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 16:31:15 +0300

    Add recursive factorial script with usage help

---
+++ 10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (arg === undefined || arg === '-h' || arg === '--help') {
+  console.log('Usage: node 10-factorial.js <integer>');
+  console.log('Computes the factorial of <integer> recursively.');
+  console.log('If no integer or NaN is provided, factorial is 1.');
+  process.exit(0);
+}
+
+const num = parseInt(arg);
+
+function factorial(n) {
+  if (isNaN(n) || n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
