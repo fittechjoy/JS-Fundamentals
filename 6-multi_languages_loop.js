8aa7c4d2ab74b7bb4ca3e97ce4315f48923e393e
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 15:38:39 +0300

    Fix syntax error

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -10,7 +10,7 @@ JavaScript is amazing
 */
 
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let result = ";
+let result = '';
 for (let i = 0; i < lines.length; i++) { result += lines[i] + '\n'; }
 console.log(result.trim());
 
