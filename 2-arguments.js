a31f9b24bffe83060613bbe55b7a0cbdedd0ab5f
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 12:40:45 +0300

    Add usage examples as comments in 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,13 @@
+
+
+#!/usr/bin/node 
+/*
+usage examples:
+$ node 2-arguments.js
+No argument
+$ node 2-arguments.js Best Argument found
+$ node 2-arguments.js Best School Argument found 
+*/
 const numArgd = process.argv.length - 2;
 if (numArgs === 0) { console.log('No argument'); }
 else if (numArgs === 1) { console.log('Argument found'); }
