cb1fd8e82738967cc9ee18ece0a166b622ab6414
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 16:00:04 +0300

    Fix output error

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,28 +1,14 @@
 #!/usr/bin/node
 
-/*
-Usage examples:
-
-$ node 7-multi_c.js 2
-C is fun
-C is fun
-
-$ node 7-multi_c.js
-Missing number of occurrences
-
-$ node 7-multi_c.js School
-Missing number of occurrences
-*/
-
 const arg = process.argv[2];
 const x = Number(arg);
 
-if (!x || isNaN(x)) {
+if (!arg || isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  let output = '';
+  let result = '';
   for (let i = 0; i < x; i++) {
-    output += 'C is fun\n';
+    result += 'C is fun\n';
   }
-  console.log(output.trimEnd());
+  console.log(result.trimEnd());
 }
