52a331eb23f04f2039d219b51aafb3903436434e
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 14:17:16 +0300

    Fix typo

--- 4-concat.js
+++ 4-concat.js
@@ -5,6 +5,6 @@ $ node 4-concat.js c c is undefined
 $ node 4-concat.js undefined is undefined
 */
 const arg1 = process.argv[2];
-const arg2 =nprocess.argv[3];
+const arg2 = process.argv[3];
 console.log('${arg1} is ${arg2}');
  
