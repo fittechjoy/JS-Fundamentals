a05caffdc08f30bdc10a0bdbc6791a797d0824f1
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 13:58:03 +0300

    Add script that prints two arguments in format <arg1> is <arg2>

---
+++ 4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+$ node 4-concat.js c cool c is cool
+$ node 4-concat.js c c is undefined
+$ node 4-concat.js undefined is undefined
+*/
+const arg1 = process.argv[2];
+const arg2 =nprocess.argv[3];
+console.log('${arg1} is ${arg2}');
+ 
