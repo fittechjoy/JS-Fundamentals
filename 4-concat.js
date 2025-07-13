e1a19b528585b37b79ffcec82522f548067033f5
Joy Shayan <kiamao1joy@gmail.com>
2025-07-13 14:34:17 +0300

    Recreate 4-concat.js with correct code and fix ReferenceError

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
+
 /*
-$ node 4-concat.js Python Python is fun
-$ node 4-concat.js HBTN HBTN is undefined
-$ node 4-concat.js undefined is undefined
+Usage examples:
+
+$ node 4-concat.js Python fun
+Python is fun
+
+$ node 4-concat.js HBTN
+HBTN is undefined
+
+$ node 4-concat.js
+undefined is undefined
 */
+
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
-console.log('${arg1} is ${arg2}');
- 
+
+console.log(`${arg1} is ${arg2}`);
